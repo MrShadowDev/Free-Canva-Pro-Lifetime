28f5323fee26bb16d5a290d861ba9d980bb0585b
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-03-03 17:18:12 +0100

    The changes I made include:
    Replacing asyncio.get_event_loop().run_until_complete(main()) with asyncio.run(main()).
    Increasing the timeout value to 120 seconds when waiting for the button to appear.
    Adding if statements to check if the button and href link were found before trying to click on the button or extract the href link. This prevents the IndexError that was occurring when the href link was not found.

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+# https://github.com/MrShadowDev/Free-Canva-Pro-Lifetime
+
 import asyncio
 from pyppeteer import launch
 
 async def main():
-    print("Free Canva Pro by MrShadowDev")
     # Launch a new browser instance
     browser = await launch(headless=True)
     # Create a new page
@@ -16,26 +17,32 @@ async def main():
     # Scroll down until the element with the text "Script link" is found
     await page.xpath("//h2[text()='Free Guide']")
     # Wait for the button to appear and be clickable
-    print("Trying to find canva pro for you! Please wait 60s...")
-    await page.waitForXPath("//*[@id='download']", {'visible': True, 'timeout': 70000})
+    print("Trying to find canva pro for you! Please wait 120s...")
+    await page.waitForXPath("//*[@id='download']", {'visible': True, 'timeout': 120000})
     button = await page.xpath("//*[@id='download']")
-    # Click the button that opens the new tab
-    await button[0].click()
-    # Wait for the new tab to open
-    await asyncio.sleep(5)
-    # Get the handle of the new tab
-    new_tab = (await browser.pages())[-1]
-    # Switch to the new tab
-    await new_tab.bringToFront()
-    # Extract the href link from the button
-    href_link = await new_tab.xpath("//a[text()='GET HERE']")
-    href_link = await (await href_link[0].getProperty('href')).jsonValue()
-    #Print the link of canva pro in a text file
-    with open("canva_pro_link.txt", "w") as f:
-        f.write(href_link)
-        print("Canva Pro Found!")
+    if button:
+        # Click the button that opens the new tab
+        await button[0].click()
+        # Wait for the new tab to open
+        await asyncio.sleep(5)
+        # Get the handle of the new tab
+        new_tab = (await browser.pages())[-1]
+        # Switch to the new tab
+        await new_tab.bringToFront()
+        # Extract the href link from the button
+        href_link = await new_tab.xpath("//a[text()='GET HERE']")
+        if href_link:
+            href_link = await (await href_link[0].getProperty('href')).jsonValue()
+            #Print the link of canva pro in a text file
+            with open("canva_pro_link.txt", "w") as f:
+                f.write(href_link)
+                print("Canva Pro Found!")
+        else:
+            print("Could not find the link to Canva Pro.")
+    else:
+        print("Could not find the button to download Canva Pro.")
 
     # Close the browser instance
     await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+asyncio.run(main())
