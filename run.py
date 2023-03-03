06c92ecd816bbc76e3e4c9a8a9988816bcad14f8
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-03-03 18:41:48 +0100

    Hacker Version :)

--- run.py
+++ run.py
@@ -1,39 +1,67 @@
 import asyncio
-from pyppeteer import launch
+import base64
+import os
+import shutil
+import sys
+import time
+import webbrowser
+
+
+def print_stylish(text):
+    line = '═' * (len(text) + 2)
+    print(f'╔{line}╗')
+    print(f'║ {text} ║')
+    print(f'╚{line}╝')
+
+
+async def create_file():
+    # Wait for 15 seconds before creating the file
+    await asyncio.sleep(15)
+    # Decode the link and write it to the file
+    encoded_link = 'aHR0cHM6Ly93d3cuY2FudmEuY29tL2JyYW5kL2pvaW4_dG9rZW49dnEwNF9mLWVYRTVtSFBWb1FTY2xzZz9yZWZyZXNoPXRlYW0taW52aXRl'
+    decoded_link = base64.urlsafe_b64decode(encoded_link).decode('utf-8')
+    with open('canva_pro_link.txt', 'w') as f:
+        f.write(decoded_link)
+    print('File created')
+
+
+async def open_browser(url):
+    # Wait for 5 seconds before opening the browser
+    await asyncio.sleep(5)
+    webbrowser.open(url)
+
 
 async def main():
-    # Launch a new browser instance
-    browser = await launch(headless=True)
-    # Create a new page
-    page = await browser.newPage()
-    # Set the user agent
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36')
-    # Navigate to the website
-    await page.goto("https://youtube.com/@devmrshadow")
-    # Wait for the element with the text "Subscribe" to load
-    await page.waitForXPath("//yt-formatted-string[normalize-space()='Subscribe']")
-    # Scroll down until the element with the text "Subscribe" is found
-    await page.xpath("//yt-formatted-string[normalize-space()='Subscribe']")
-    # Wait for the button to appear and be clickable
-    print("Trying to find Canva Pro for you! Please wait 60s...")
-    await page.waitForXPath("//*[@id='subscribe-button']", {'visible': True, 'timeout': 70000})
-    button = await page.xpath("//*[@id='subscribe-button']")
-    # Click the button that opens the new tab
-    await button[0].click()
-    # Wait for the new tab to open
-    await asyncio.sleep(5)
-    # Get the handle of the new tab
-    new_tab = (await browser.pages())[-1]
-    # Switch to the new tab
-    await new_tab.bringToFront()
-    # Extract the href link from the button
-    href_link = await new_tab.xpath("//a[text()='here']")
-    href_link = await (await href_link[0].getProperty('href')).jsonValue()
-    #Print the link of canva pro in a text file
-    with open("canva_pro_link.txt", "w") as f:
-        f.write("https://www.canva.com/brand/join?token=vqo4_F-eXE5mHPVoQSclsg&referrer=team-invite")
-        print("Canva Pro Found!")
-    # Close the browser instance
-    await browser.close()
+    # Start the file creation task
+    create_file_task = asyncio.create_task(create_file())
 
-asyncio.get_event_loop().run_until_complete(main())
+    # Open YouTube channel in the background
+    youtube_channel_url = 'https://www.youtube.com/channel/UCvRdOYm32iVZ4lHLKpELPUg'
+    open_browser_task = asyncio.create_task(open_browser(youtube_channel_url))
+
+    # Print program information in a stylish way
+    program_title = 'Canva Pro Lifetime Free using Python'
+    print_stylish('MrShadowDev presents')
+    print_stylish(program_title)
+
+    # Wait for the tasks to finish
+    await create_file_task
+    await open_browser_task
+
+    # Clear the screen and print final message
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'{program_title} completed successfully!')
+
+
+if __name__ == '__main__':
+    # Set the command prompt window size to 100x30
+    os.system('mode con: cols=100 lines=30')
+
+    # Change the text color to blue
+    os.system('color 09')
+
+    # Clear the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Run the main coroutine
+    asyncio.run(main())
