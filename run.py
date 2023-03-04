db8fad02749c3acf790d3423e752f0b0bcb95e84
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-03-04 19:11:06 +0100

    Fixed + Working Canva

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import shutil
 import sys
 import time
 import webbrowser
+import tkinter as tk
 
 
 def print_stylish(text):
@@ -18,7 +19,7 @@ async def create_file():
     # Wait for 15 seconds before creating the file
     await asyncio.sleep(15)
     # Decode the link and write it to the file
-    encoded_link = 'aHR0cHM6Ly93d3cuY2FudmEuY29tL2JyYW5kL2pvaW4_dG9rZW49dnEwNF9mLWVYRTVtSFBWb1FTY2xzZz9yZWZyZXNoPXRlYW0taW52aXRl'
+    encoded_link = 'aHR0cHM6Ly9iaXQubHkvT25oYXhwa19uZXRfQ2FudmFfRmViMjE='
     decoded_link = base64.urlsafe_b64decode(encoded_link).decode('utf-8')
     with open('canva_pro_link.txt', 'w') as f:
         f.write(decoded_link)
@@ -53,6 +54,31 @@ async def main():
     print(f'{program_title} completed successfully!')
 
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.title_label = tk.Label(self, text='Canva Pro Lifetime Free using Python', font=('Arial', 16))
+        self.title_label.pack(pady=10)
+
+        self.start_button = tk.Button(self, text='Start', font=('Arial', 12), command=self.start_program)
+        self.start_button.pack(pady=10)
+
+        self.quit_button = tk.Button(self, text='Quit', font=('Arial', 12), command=self.master.destroy)
+        self.quit_button.pack(pady=10)
+
+    def start_program(self):
+        # Start the main coroutine in a new thread
+        asyncio_thread = threading.Thread(target=asyncio.run, args=(main(),))
+        asyncio_thread.start()
+        # Disable the start button to prevent multiple program runs
+        self.start_button.config(state='disabled')
+
+
 if __name__ == '__main__':
     # Set the command prompt window size to 100x30
     os.system('mode con: cols=100 lines=30')
@@ -63,5 +89,10 @@ if __name__ == '__main__':
     # Clear the screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    # Run the main coroutine
+    # Create the GUI window
+    root = tk.Tk()
+    root.title('Canva Pro Lifetime Free using Python')
+    app = Application(master=root)
+
+    # Run the GUI loop
     asyncio.run(main())
