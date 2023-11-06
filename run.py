2f2969a5c44e0fffe0e63e3d8cb7543003312d53
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-11-06 08:31:58 +0100

    ~
    Fixed

--- run.py
+++ run.py
@@ -1,46 +1,42 @@
 import asyncio
 import base64
 import os
-import shutil
-import sys
-import time
 import tkinter as tk
 
-
-def print_stylish(text):
-    line = '═' * (len(text) + 2)
-    print(f'╔{line}╗')
-    print(f'║ {text} ║')
-    print(f'╚{line}╝')
-
-
-async def create_file():
-    # Wait for 15 seconds before creating the file
-    await asyncio.sleep(15)
-    # Decode the link and write it to the file
-    encoded_link = 'aHR0cHM6Ly9saW5rLWh1Yi5uZXQvOTkwMjExL3dvcmtpbmctY2FudmEtcHJvLWludml0ZQ=='
-    decoded_link = base64.urlsafe_b64decode(encoded_link).decode('utf-8')
-    with open('canva_pro_link.txt', 'w') as f:
-        f.write(decoded_link)
-    print('File created')
-
+def create_file():
+    try:
+        # Decode the link
+        encoded_link = 'aHR0cHM6Ly9saW5rLWh1Yi5uZXQvOTkwMjExL3dvcmtpbmctY2FudmEtcHJvLWludml0ZQ=='
+        decoded_link = base64.urlsafe_b64decode(encoded_link).decode('utf-8')
+        
+        # Get the user's home directory
+        user_home = os.path.expanduser("~")
+        
+        # Create the 'CanvaPro' directory in the user's home directory
+        canva_pro_dir = os.path.join(user_home, 'CanvaPro')
+        os.makedirs(canva_pro_dir, exist_ok=True)
+        
+        # Create the 'canva_pro_link.txt' file in the 'CanvaPro' directory
+        file_path = os.path.join(canva_pro_dir, 'canva_pro_link.txt')
+        
+        # Write the decoded link to the file
+        with open(file_path, 'w') as f:
+            f.write(decoded_link)
+        
+        print('File created successfully.')
+    except Exception as e:
+        print(f'Error creating the file: {e}')
 
 async def main():
-    # Start the file creation task
-    create_file_task = asyncio.create_task(create_file())
-
-    # Print program information in a stylish way
+    # Print program information
     program_title = 'Canva Pro Lifetime Free using Python'
     print_stylish('MrShadowDev presents')
     print_stylish(program_title)
-
-    # Wait for the task to finish
-    await create_file_task
-
-    # Clear the screen and print the final message
-    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    # Call the create_file function
+    create_file()
+    
     print(f'{program_title} completed successfully!')
-
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -53,34 +49,23 @@ class Application(tk.Frame):
         self.title_label = tk.Label(self, text='Canva Pro Lifetime Free using Python', font=('Arial', 16))
         self.title_label.pack(pady=10)
 
-        self.start_button = tk.Button(self, text='Start', font=('Arial', 12), command=self.start_program)
+        self.start_button = tk.Button(self, text='Create File', font=('Arial', 12), command=self.start_program)
         self.start_button.pack(pady=10)
 
         self.quit_button = tk.Button(self, text='Quit', font=('Arial', 12), command=self.master.destroy)
         self.quit_button.pack(pady=10)
 
     def start_program(self):
-        # Start the main coroutine in a new thread
-        asyncio_thread = threading.Thread(target=asyncio.run, args=(main(),))
-        asyncio_thread.start()
-        # Disable the start button to prevent multiple program runs
+        # Disable the 'Create File' button to prevent multiple file creation
         self.start_button.config(state='disabled')
+        # Call the create_file function
+        create_file()
 
-
-if __name__ == '__main__':
-    # Set the command prompt window size to 100x30
-    os.system('mode con: cols=100 lines=30')
-
-    # Change the text color to blue
-    os.system('color 09')
-
-    # Clear the screen
-    os.system('cls' if os.name == 'nt' else 'clear')
-
+if __name__ == '__main':
     # Create the GUI window
     root = tk.Tk()
     root.title('Canva Pro Lifetime Free using Python')
     app = Application(master=root)
 
     # Run the GUI loop
-    asyncio.run(main())
+    app.mainloop()
